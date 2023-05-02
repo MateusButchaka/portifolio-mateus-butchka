96ff69622c09fa1451291ee93416cd9f92e9ee42
mateus butchaka <mateusgotardi06@gmail.com>
2023-05-02 20:15:02 -0300

    adicionando animação inicio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
-// const navFix = document.querySelector('.nav-fix');
-// setTimeout(() => {
-//   navFix.classList.add('animate-nav');
-// }, 50000);
+// seleciona a imagem
+const minhaImagem = document.getElementById("imgsobre");
+
+// detecta a posição da imagem em relação ao topo da página
+const imagemPosicao = minhaImagem.offsetTop;
+
+// adiciona um listener para o evento de scroll
+window.addEventListener('scroll', function handler() {
+  // detecta a posição atual do scroll da página
+  const scrollPosicao = window.pageYOffset;
+
+  // verifica se a posição do scroll é maior ou igual à posição da imagem
+  if (scrollPosicao >= imagemPosicao) {
+    // adiciona a classe "visivel" à imagem
+    minhaImagem.classList.add("visivel");
+
+    // remove o listener de scroll
+    window.removeEventListener('scroll', handler);
+  }
+});
