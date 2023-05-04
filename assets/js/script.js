08ad1df31ead429d0ec4bbb7d14ebd37f2cd4c9e
mateus butchaka <mateusgotardi06@gmail.com>
2023-05-03 21:06:35 -0300

    animções sem atraso

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 const imagens = document.querySelectorAll('.imgsobre, .imgprojetos, .imghabilidades');
-
-function debounce(func, wait = 15, immediate = true) {
+const debounce = function(func, wait = 10, immediate = true) {
   let timeout;
   return function() {
     const context = this, args = arguments;
@@ -16,25 +15,15 @@ function debounce(func, wait = 15, immediate = true) {
 }
 
 function handleScroll() {
-  const scrollPosicao = window.pageYOffset + window.innerHeight;
-  
   imagens.forEach(imagem => {
-    const imagemPosicao = imagem.dataset.posicao;
-    
+    const imagemPosicao = imagem.getBoundingClientRect().top + window.pageYOffset;
+    const scrollPosicao = window.pageYOffset + window.innerHeight - imagem.offsetHeight / 2;
     if (scrollPosicao >= imagemPosicao) {
       imagem.classList.add("visivel");
     }
   });
 }
 
-function calcularPosicaoImagens() {
-  imagens.forEach(imagem => {
-    const imagemPosicao = imagem.offsetTop;
-    imagem.dataset.posicao = imagemPosicao;
-  });
-}
-
 const handleScrollDebounced = debounce(handleScroll);
 
 window.addEventListener('scroll', handleScrollDebounced);
-window.addEventListener('DOMContentLoaded', calcularPosicaoImagens);
