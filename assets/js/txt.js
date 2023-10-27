d35c408ff1b75fcdb79892cd790ed82c308dd861
MateusButchaka <mateusgotardi06@gmail.com>
2023-10-27 13:35:55 -0300

    alteracoes e finalizacao

---
+++ assets/js/txt.js
@@ -0,0 +1,14 @@
+const text = "Mateus Gotardi Butchaka"; 
+const typingSpan = document.querySelector(".typing-text");
+
+function typeText() {
+    let i = 0;
+    const interval = setInterval(function() {
+        typingSpan.textContent += text[i];
+        i++;
+        if (i === text.length) {
+            clearInterval(interval);
+        }
+    }, 200); 
+}
+typeText();
