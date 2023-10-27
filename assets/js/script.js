95c8ab4ec1510a630b5117d757f24ad387110e6c
MateusButchaka <mateusgotardi06@gmail.com>
2023-10-27 13:52:15 -0300

    arrumando bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,16 @@ $(document).ready(function(){
 
   $('.scroll-top').hide();
 
-  /*--------------- Navbar Toggler ---------------*/
   $('#menu-btn').click(function(){
     $(this).toggleClass('fa-times');
     $('.navbar').toggleClass('active');
   });
 
-  /*--------------- Scroll-Top ---------------*/
   $(window).on('scroll',function(){
     
     $('#menu-btn').removeClass('fa-times');
     $('.navbar').removeClass('active');
 
-    // STICKY HEADER
     if($(window).scrollTop() > 0){
       $(".header").addClass("sticky");
     }else{
